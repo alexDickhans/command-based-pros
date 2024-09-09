24099348ec811b11590384d22dbe4ac5047ac489
Alex Dickhans <adickhans@gmail.com>
2024-09-09 17:49:55 -0600

    Document ProxyCommand

--- include/command/instantCommand.h
+++ include/command/instantCommand.h
@@ -11,7 +11,7 @@ public:
 	 * Create a new InstantCommand
 	 *
 	 * @param on_init Function to run once upon function start
-	 * @param requirements \refitem Subsystem requirements for this command
+	 * @param requirements Subsystem requirements for this command
 	 */
 	InstantCommand(const std::function<void()> &on_init, const std::initializer_list<Subsystem *> &requirements)
 		: FunctionalCommand(on_init, [] {}, [](bool _) {}, [] { return true; } , requirements) {

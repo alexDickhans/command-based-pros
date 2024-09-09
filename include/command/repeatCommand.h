c1041d2034e1f591aba4b05563d09eeadb7aa098
Alex Dickhans <adickhans@gmail.com>
2024-09-09 17:57:44 -0600

    Document RepeatCommand

--- include/command/repeatCommand.h
+++ include/command/repeatCommand.h
@@ -1,18 +1,33 @@
 #pragma once
+
 #include "command.h"
 
+/**
+ * @brief Makes a \refitem Command repeat each time after it is run
+ */
 class RepeatCommand : public Command {
 private:
 	Command* command;
 public:
-	RepeatCommand(Command* command) {
+	/**
+	 * @brief Create a RepeatCommand with a \refitem Command pointer
+	 *
+	 * @param command The \refitem Command to make into a RepeatCommand
+	 */
+	explicit RepeatCommand(Command* command) {
 		this->command = command;
 	}
 
+	/**
+	 * @brief Just initializes the \refitem Command
+	 */
 	void initialize() override {
 		command->initialize();
 	}
 
+	/**
+	 * @brief Executes the \refitem Command, if it is finished it restarts the \refitem Command
+	 */
 	void execute() override {
 		command->execute();
 
@@ -22,10 +37,20 @@ public:
 		}
 	}
 
+	/**
+	 * @brief Ends the \refitem Command with interrupted set to true
+	 *
+	 * @param interrupted Ignored, if the command is ended it must be interrupted because it is always restarting
+	 */
 	void end(bool interrupted) override {
 		command->end(true);
 	}
 
+	/**
+	 * @brief Passes on the requirements of the \refitem Command that was passed in
+	 *
+	 * @return The requirements of the \refitem Command that was passed in
+	 */
 	std::vector<Subsystem *> getRequirements() override {
 		return std::move(command->getRequirements());
 	}

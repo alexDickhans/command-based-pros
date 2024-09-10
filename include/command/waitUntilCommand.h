be573055677a590ebef512531df3f4f401af56b5
Alex Dickhans <adickhans@gmail.com>
2024-09-09 19:58:54 -0600

    Wait until command

--- include/command/waitUntilCommand.h
+++ include/command/waitUntilCommand.h
@@ -2,8 +2,16 @@
 
 #include "functionalCommand.h"
 
+/**
+ * @brief WaitUntilCommand creates a command that ends once a condition is finished. This command has no requirements.
+ */
 class WaitUntilCommand : public FunctionalCommand {
 public:
+	/**
+	 * @brief Create a new WaitUntilCommand object with an isFinish conditional
+	 *
+	 * @param is_finish The conditional to end with. Once it is true the command will finish
+	 */
 	WaitUntilCommand(const std::function<bool()> &is_finish)
 		: FunctionalCommand([]() {}, []() {}, [](bool _) {}, is_finish, {}) {
 	}
